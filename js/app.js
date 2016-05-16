386ee8e06e5bc56c997808a52c9a5fdcbc2de92b
Adrian J Lopez <adrianjaylopez@gmail.com>
2016-05-16 12:49:12 -0400

    Added new game function, added setrandomnumber function inside newgame, added new game function to app.js.

--- js/app.js
+++ js/app.js
@@ -1,17 +1,37 @@
-
 $(document).ready(function(){
-	
+	var game;
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
-
   	});
-
   	/*--- Hide information modal box ---*/
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+		/** start game and retain state*/
+		game = newGame();
+		game.setRandomNumber();
+		/** create new game object and generate new random number */
+		$('.new').click(function() {
+			game = newGame();
+			game.setRandomNumber();
+		});
+		$('#guessButton').submit(function() {
 
+		});
 });
-
-
+/**
+* function to start a new game
+*/
+function newGame() {
+	var randomNumber = 0;
+	var gameState = {};
+	/**
+	* function to generate random number from 1 to 100
+	* return random number
+	*/
+	gameState.setRandomNumber = function() {
+		randomNumber = Math.floor((Math.random() * 100) + 1);
+	};
+	return gameState;
+}
